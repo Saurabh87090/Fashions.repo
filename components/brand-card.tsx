928b85a017dd364574c1fccdf795b4f860704572
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2024-08-05 19:34:11 +0530

    Update brand-card.tsx

--- components/brand-card.tsx
+++ components/brand-card.tsx
@@ -8,6 +8,7 @@ function Brand(props: any) {
     whileHover={{ scale: 1.1 }}
           
     transition={{ type: 'spring', stiffness: 400, damping: 10 }}>
+        
     <div className='flex flex-row items-center justify-center md:flex-row bg-[#F6F6F6] h-[260px] w-[350px] md:w-[500px] my-5 p-5 rounded-2xl md:my-10 md:mx-5'>
         <img className='w-[150px] h-[150px] md:w-[180px] md:h-[180px] my-2 md:my-0 mx-auto md:mx-8 ' src={imgSrc} alt={name} />
         <div className='text-left mx-5 my-2 md:mx-9 md:my-8'>
@@ -15,6 +16,7 @@ function Brand(props: any) {
           <p className='text-[#8B8E99] mt-2 text-sm md:text-base'>{content}</p>
         </div>
       </div>
+        
     </motion.div>
   )
 }
