041c9d3f72b01c6e047c5469b7b5fa55a26d0ba3
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2024-08-05 19:34:32 +0530

    Update Star.tsx

--- components/Star.tsx
+++ components/Star.tsx
@@ -4,12 +4,14 @@ function Star({ star }: { star: number }) {
   return (
     <div className='flex space-x-1'>
       {Array.from({ length: star }, (_, index) => (
+      
         <img
           key={index}
           src="/star.svg" // replace with the path to your star image
           alt="star"
           className="star-image" // optional: add a class for styling
         />
+      
       ))}
     </div>
   );
