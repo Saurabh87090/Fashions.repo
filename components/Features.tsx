951c9ec69616f85e03ffe0dd005dae3b615ad0f6
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2024-08-06 23:05:35 +0530

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -5,7 +5,6 @@ import { brandData } from "@/app/data/brand";
 const Features = () => {
   return (
     <div>
-      
       <div className="flex flex-col items-center justify-center md:flex-row flex-wrap ">
         {brandData.map((brand, index) => (
           <Brand
