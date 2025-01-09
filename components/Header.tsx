caaccee474ca5c453987988f9e334c596e7cd993
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2025-01-09 08:22:34 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ const Header = () => {
   };
 
   return (
+    
     <div>
       <header>
         <div className={`relative bg-gradient-to-r from-gray-900 to-black  ${menuOpen ? 'h-screen' : 'h-auto'}`}>
